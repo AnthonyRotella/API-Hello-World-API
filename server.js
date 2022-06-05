b08626ffbcdd0bba1c55e661d79f45e784395f68
Anthony Rotella <mr4406@yahoo.com>
2022-06-05 08:24:43 -0500

    Setup

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
 // MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // ROUTES
 app.get('/', (req, res) => {
